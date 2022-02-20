6f7ad193e631921ec29cd65e17418dee12503307
Darya Lomako <lomakodasha@gmail.com>
2022-02-20 22:40:12 +0300

    feat: add constants to define types of figures

--- solution.go
+++ solution.go
@@ -4,38 +4,34 @@ import (
 	"math"
 )
 
-// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
-
-// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-// it's like:
-// CalcSquare(10.0, SidesTriangle)
-// CalcSquare(10.0, SidesSquare)
-// CalcSquare(10.0, SidesCircle)
-
 type amountOfSides int
+
+const (
+	SidesTriangle amountOfSides = 3
+	SidesSquare   amountOfSides = 4
+	SidesCircle   amountOfSides = 0
+)
 
 func CalcSquare(sideLen float64, sidesNum amountOfSides) float64 {
 	var figureSquare float64
 
 	switch sidesNum {
-	case 0:
+	case SidesTriangle:
 		{
 			figureSquare = math.Pi * math.Pow(sideLen, 2)
-			return figureSquare
 		}
-	case 3:
+	case SidesSquare:
 		{
 			figureSquare = math.Pi * sideLen * sideLen
-			return figureSquare
 		}
-	case 4:
+	case SidesCircle:
 		{
 			figureSquare = (math.Sqrt(3) * sideLen * sideLen) / 4
-			return figureSquare
 		}
 	default:
 		{
-			return 0
+			figureSquare = 0
 		}
 	}
+	return figureSquare
 }
